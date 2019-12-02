59af8665f166b9ffde06431bd08ad705052fe411
maik123456789 <54273123+maik123456789@users.noreply.github.com>
2019-12-02 19:50:40 +0100

    added check Token. polling needs revision

--- NetworkMode.py
+++ NetworkMode.py
@@ -7,7 +7,7 @@ import time
 def polling(sorce,dest1,dest2,payload,timeout):
     ID=FancyDataSender.BaseDataSender.address
     max_time = time.time() + timeout
-    
+    check = False
     for x in range(1,8) :
         dest1 = x
         dest2 = x
@@ -21,4 +21,16 @@ def polling(sorce,dest1,dest2,payload,timeout):
             if time_actual >=  time_first + 5 :
                 break
             if response is not None :
+                check = check_reply()
                 break
+    if check == False :
+        check_token()
+
+def check_reply() :
+    data=PacketManager.readNetworkPacket[6]
+    if data is CTE.NETWORK_PAQUET_CONTROL_REPLY_YES_PAYLOAD :
+        return True
+
+def check_token() :
+###Falta función del token para poder checkearlo###
+    
