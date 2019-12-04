81b8e69688b312e1f9347e015c236d3fb9ed8301
estebanfex <estebanfex@gmail.com>
2019-12-04 18:54:54 +0100

    Update constants
    TeamA: Update constants and improved passive node

--- passive_node.py
+++ passive_node.py
@@ -1,35 +1,42 @@
 import Support.Constants as CTE
+import Utils.PacketManager as PM
 import time
 import random
 import Tx as tx
-from Utils.PacketManager import readNetworkPacket, createNetworkPacket
-from ModeControllers.NetworkMode import isFileAlreadyReceived
 
+import NetworkMode as NM
 
-TIMEOUT_MAX2SEND = 1000   # one second
+TIMEOUT_MAX2SEND = 1000   # one second range
 
-def check_recive_hello(receiver):
-    ret = False
-    msg_rcv = receiver.receivePacket
-    payload, source, dest1, dest2, isACK, sequenceNumber, packetType = readNetworkPacket(msg_rcv)
-
-    if (packetType == CTE.NETWORK_PAQUET_TYPE_CONTROL & payload == CTE.NETWORK_PAQUET_CONTROL_HELLO_PAYLOAD):
-        ret = True
-
-    return ret, source
 
 def passive_node(transmiter, receiver, hist):
-    FileAlreadyReceived = isFileAlreadyReceived()
-    IsHello, NextRcv = check_recive_hello( receiver )
-    if( IsHello ):
-        if( FileAlreadyReceived ):
-            #reply NO
-            time.sleep(random.randint(0, TIMEOUT_MAX2SEND) / 1000.0)
-            packet = createNetworkPacket(CTE.NETWORK_PAQUET_CONTROL_REPLY_NO_PAYLOAD, CTE.IP_ADDRESS, NextRcv, NextRcv, CTE.PAQUET_FIELD_ISACK_NO_ACK, CTE.PAQUET_FIELD_SN_FIRST, CTE.NETWORK_PAQUET_TYPE_CONTROL)
-        else:
-            # reply YES
-            time.sleep(random.randint(0,TIMEOUT_MAX2SEND)/1000.0)
-            packet = createNetworkPacket(CTE.ETWORK_PAQUET_CONTROL_REPLY_YES_PAYLOAD, CTE.IP_ADDRESS, NextRcv, NextRcv, CTE.PAQUET_FIELD_ISACK_YES_ACK, CTE.PAQUET_FIELD_SN_FIRST, CTE.NETWORK_PAQUET_TYPE_CONTROL)
-        tx.sendPacket(packet)
+    ChangeToActiveNode = False
+    FileAlreadyReceived = NM.isFileAlreadyReceived()
 
-    return FileAlreadyReceived
+    msg_rcv = receiver.receivePacket
+    payload, next_rcv, dest1, dest2, isACK, sequenceNumber, packetType = PM.readNetworkPacket(msg_rcv)
+
+    if packetType == CTE.NETWORK_PAQUET_TYPE_CONTROL:
+
+        if payload == CTE.NETWORK_PAQUET_CONTROL_HELLO_PAYLOAD:
+            if FileAlreadyReceived:
+                packet = PM.createReplyNoPacket(next_rcv)
+                time.sleep(random.randint(0, TIMEOUT_MAX2SEND) / 1000.0)
+            else:
+                packet = PM.createReplyYesPacket(next_rcv)
+                time.sleep(random.randint(0,TIMEOUT_MAX2SEND)/1000.0)
+            tx.sendPacket(packet)
+
+
+    if packetType == CTE.NETWORK_PAQUET_TYPE_PASSTOKEN:
+
+        if dest1 == dest2:
+            if FileAlreadyReceived:
+                packet = PM.createReplyYesPacket(next_rcv)
+                ChangeToActiveNode = True
+            else:
+                packet = PM.createReplyNoPacket(next_rcv)
+            tx.sendPacket(packet)
+        else:
+            NM.pass_token(dest2, tx)
+    return ChangeToActiveNode
